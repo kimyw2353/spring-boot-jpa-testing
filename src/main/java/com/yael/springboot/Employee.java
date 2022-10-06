5704f536fb2e52599cc5c75ff0115333a6fc9157
kimyw2353 <kimyw2353@naver.com>
2022-10-06 19:39:00 +0900

    entity 생성

---
+++ src/main/java/com/yael/springboot/Employee.java
@@ -0,0 +1,35 @@
+package com.yael.springboot;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data //getter, setter 만들어줌
+@AllArgsConstructor // 필드에 쓴 모든 생성자 만들어줌
+@NoArgsConstructor //기본생성자를 만들어줌
+@Entity
+@Table(name = "employees")
+
+public class Employee {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY) //IDENTITY는 기본 키 생성을 DB에 위임한다는 설정
+	private long id;
+	
+	@Column(name = "first_name", nullable = false)
+	private String firstName;
+	
+	@Column(name = "last_name", nullable = false)
+	private String lastName;
+	
+	@Column(name = "email", nullable = false)
+	private String email;
+
+}
