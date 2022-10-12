33e3e43b0dc286ffd4d15510589f418938aba636
kimyw2353 <kimyw2353@naver.com>
2022-10-12 18:03:18 +0900

    getList, update Test 작성

--- src/test/java/com/yael/springboot/EmployeeRepositoryTest.java
+++ src/test/java/com/yael/springboot/EmployeeRepositoryTest.java
@@ -2,6 +2,8 @@ package com.yael.springboot;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -25,6 +27,37 @@ public class EmployeeRepositoryTest {
 		employeeRepository.save(employee);
 		
 		assertThat(employee.getId()).isGreaterThan(0);
+	}
+	
+	@Test
+	public void getEmployeeTest() {
+		
+		Employee employee = employeeRepository.findById(1L).get();
+		
+		assertThat(employee.getId()).isEqualTo(1L);
+		
+	}
+	
+	@Test
+	public void getListOfEmployeeTest() {
+		
+		List<Employee> employees = employeeRepository.findAll();
+		
+		assertThat(employees.size()).isGreaterThan(0);
+		
+	}
+	
+	@Test
+	public void updateEmployeeTest() {
+		
+		Employee employee = employeeRepository.findById(1L).get();
+		
+		employee.setEmail("kimyw2353@gmail.com");
+		
+		Employee employeeUpdated = employeeRepository.save(employee);
+		
+		assertThat(employeeUpdated.getEmail()).isEqualTo("kimyw2353@gmail.com");
+		
 	}
 	
 }
